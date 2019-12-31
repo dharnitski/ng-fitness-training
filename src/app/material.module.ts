1e564c90f88bd5ce413bee659f2c5fe0fcc76d46
Dmitry Harnitski <dharnitski@gmail.com>
2019-12-30 23:29:19 -0500

    data picker age and checkbox

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import {
     MatButtonModule, MatIconModule, MatInputModule,
-    MatFormFieldModule, MatDatepickerModule, MatNativeDateModule
+    MatFormFieldModule, MatDatepickerModule, MatNativeDateModule,
+    MatCheckboxModule
 } from '@angular/material';
 
 const types = [MatButtonModule, MatIconModule, MatInputModule,
-    MatFormFieldModule, MatDatepickerModule, MatNativeDateModule];
+    MatFormFieldModule, MatDatepickerModule, MatNativeDateModule,
+    MatCheckboxModule
+];
 
 @NgModule({
     imports: types,
